a4506b2c4c871f5fee1deaeaa0de822a9b8f6fd3
haythem05 <haythem.loueti@esprit.tn>
2025-07-15 17:23:35 +0100

    changed apiurl varible

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://backend.local'
+    apiUrl: 'http://gateway.local'
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://backend.local'
+    apiUrl: 'http://gateway.local'
   };
